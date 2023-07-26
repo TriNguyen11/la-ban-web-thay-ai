5ea10d04d964b0a58345f6656052fa699b41bf86
Apple <trinq110902@gmail.com>
2023-07-26 15:53:30 +0700

    asd

--- src/views/CreateNewProject.js
+++ src/views/CreateNewProject.js
@@ -8,89 +8,39 @@ let accelerometer = null;
 const SHAKE_THRESHOLD = 10;
 
 function CreateNewProject(props) {
-  const compassCircle = document.querySelector(".compass-circle");
-  const myPoint = document.querySelector(".my-point");
-  const startBtn = document.querySelector(".start-btn");
-  const isIOS =
-    navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-    navigator.userAgent.match(/AppleWebKit/);
-
-  function init() {
-    startBtn?.addEventListener("click", startCompass);
-    navigator.geolocation.getCurrentPosition(locationHandler);
-
-    if (!isIOS) {
-      window.addEventListener("deviceorientationabsolute", handler, true);
-    }
-  }
-
-  function startCompass() {
-    if (isIOS) {
-      DeviceOrientationEvent.requestPermission()
-        .then((response) => {
-          if (response === "granted") {
-            window.addEventListener("deviceorientation", handler, true);
-          } else {
-            alert("has to be allowed!");
-          }
-        })
-        .catch(() => alert("not supported"));
-    }
-  }
-
-  function handler(e) {
-    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-    compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
-
-    // ±15 degree
-    if (
-      (pointDegree < Math.abs(compass) &&
-        pointDegree + 15 > Math.abs(compass)) ||
-      pointDegree > Math.abs(compass + 15) ||
-      pointDegree < Math.abs(compass)
-    ) {
-      myPoint.style.opacity = 0;
-    } else if (pointDegree) {
-      myPoint.style.opacity = 1;
-    }
-  }
-
-  let pointDegree;
-
-  function locationHandler(position) {
-    const { latitude, longitude } = position.coords;
-    pointDegree = calcDegreeToPoint(latitude, longitude);
-
-    if (pointDegree < 0) {
-      pointDegree = pointDegree + 360;
-    }
-  }
-
-  function calcDegreeToPoint(latitude, longitude) {
-    // Qibla geolocation
-    const point = {
-      lat: 21.422487,
-      lng: 39.826206,
-    };
-
-    const phiK = (point.lat * Math.PI) / 180.0;
-    const lambdaK = (point.lng * Math.PI) / 180.0;
-    const phi = (latitude * Math.PI) / 180.0;
-    const lambda = (longitude * Math.PI) / 180.0;
-    const psi =
-      (180.0 / Math.PI) *
-      Math.atan2(
-        Math.sin(lambdaK - lambda),
-        Math.cos(phi) * Math.tan(phiK) -
-          Math.sin(phi) * Math.cos(lambdaK - lambda)
+  document.addEventListener("DOMContentLoaded", function (event) {
+    if (window.DeviceOrientationEvent) {
+      document.getElementById("notice").innerHTML = "Working API detected";
+      window.addEventListener(
+        "deviceorientation",
+        (eventData) => {
+          // gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
+          const tiltLR = eventData.gamma;
+          // beta: Tilting the device from the front to the back. Tilting the device to the front will result in a positive value.
+          const tiltFB = eventData.beta;
+          // alpha: The direction the compass of the device aims to in degrees.
+          const dir = eventData.alpha;
+          // Call the function to use the data on the page.
+          deviceOrientationHandler(tiltLR, tiltFB, dir);
+        },
+        false
       );
-    return Math.round(psi);
-  }
+    } else {
+      document.getElementById("notice").innerHTML = "No API detected";
+    }
 
-  init();
-  setTimeout(() => {
-    init();
-  }, 1000);
+    function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+      document.getElementById("tiltLR").innerHTML = Math.ceil(tiltLR);
+      document.getElementById("tiltFB").innerHTML = Math.ceil(tiltFB);
+      document.getElementById("direction").innerHTML = Math.ceil(dir);
+      // Rotate the disc of the compass. - CSS transform
+      const compassDisc = document.getElementById("compassDiscImg");
+      compassDisc.style.transform = `rotate(${dir}deg)`;
+      compassDisc.style.webkitTransform = `rotate(${dir}deg)`;
+      compassDisc.style.MozTransform = `rotate(${dir}deg)`;
+    }
+  });
+
   return (
     // <>
     //   <Image
@@ -490,12 +440,24 @@ function CreateNewProject(props) {
     //   </main> */}
     // </>
     <>
+      <h1>Compass</h1>
       <div class="compass">
         <div class="arrow"></div>
-        <div class="compass-circle"></div>
-        <div class="my-point"></div>
+        <div class="disc" id="compassDiscImg"></div>
       </div>
-      <button class="start-btn">Start compass</button>
+
+      <div class="orientation-data">
+        <div>
+          Beta: <span id="tiltFB"></span>
+        </div>
+        <div>
+          Gamma: <span id="tiltLR"></span>
+        </div>
+        <div>
+          Alpha: <span id="direction"></span>
+        </div>
+      </div>
+      <div id="notice"></div>
     </>
   );
 }
