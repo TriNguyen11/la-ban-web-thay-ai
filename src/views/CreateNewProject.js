aacbc82083748709fd025b38cdeeddb04dd752a7
Apple <trinq110902@gmail.com>
2023-07-27 10:46:25 +0700

    asd

--- src/views/CreateNewProject.js
+++ src/views/CreateNewProject.js
@@ -34,74 +34,6 @@ function CreateNewProject(props) {
       accelerometer.unsubscribe();
     }
   };
-  function compassHeading(alpha, beta, gamma) {
-    // Convert degrees to radians
-    var alphaRad = alpha * (Math.PI / 180);
-    var betaRad = beta * (Math.PI / 180);
-    var gammaRad = gamma * (Math.PI / 180);
-
-    // Calculate equation components
-    var cA = Math.cos(alphaRad);
-    var sA = Math.sin(alphaRad);
-    var cB = Math.cos(betaRad);
-    var sB = Math.sin(betaRad);
-    var cG = Math.cos(gammaRad);
-    var sG = Math.sin(gammaRad);
-
-    // Calculate A, B, C rotation components
-    var rA = -cA * sG - sA * sB * cG;
-    var rB = -sA * sG + cA * sB * cG;
-    var rC = -cB * cG;
-
-    // Calculate compass heading
-    var compassHeading = Math.atan(rA / rB);
-
-    // Convert from half unit circle to whole unit circle
-    if (rB < 0) {
-      compassHeading += Math.PI;
-    } else if (rA < 0) {
-      compassHeading += 2 * Math.PI;
-    }
-
-    // Convert radians to degrees
-    compassHeading *= 180 / Math.PI;
-
-    return compassHeading;
-  }
-  async function compassHeading(alpha, beta, gamma) {
-    // Convert degrees to radians
-    var alphaRad = alpha * (Math.PI / 180);
-    var betaRad = beta * (Math.PI / 180);
-    var gammaRad = gamma * (Math.PI / 180);
-
-    // Calculate equation components
-    var cA = Math.cos(alphaRad);
-    var sA = Math.sin(alphaRad);
-    var cB = Math.cos(betaRad);
-    var sB = Math.sin(betaRad);
-    var cG = Math.cos(gammaRad);
-    var sG = Math.sin(gammaRad);
-
-    // Calculate A, B, C rotation components
-    var rA = -cA * sG - sA * sB * cG;
-    var rB = -sA * sG + cA * sB * cG;
-    var rC = -cB * cG;
-
-    // Calculate compass heading
-    var compassHeading = Math.atan(rA / rB);
-
-    // Convert from half unit circle to whole unit circle
-    if (rB < 0) {
-      compassHeading += Math.PI;
-    } else if (rA < 0) {
-      compassHeading += 2 * Math.PI;
-    }
-
-    // Convert radians to degrees
-    compassHeading *= 180 / Math.PI;
-
-    return compassHeading;
-  }
   async function handleOrientation(event) {
     updateFieldIfNotNull("z", event.alpha);
     updateFieldIfNotNull("x", event.beta);
@@ -112,13 +44,9 @@ function CreateNewProject(props) {
       ["x"]: event.beta.toFixed(10),
       ["y"]: event.gamma.toFixed(10),
     });
-
     let tmp =
       90 -
       todos.caculateAngle({ x: event.beta, y: event.gamma, z: event.alpha });
-    var a1, a2, b1, b2;
-    let angle;
-    // let { x, y, z } = magnetometer;
     if (tmp < 0) {
       tmp = 360 + tmp;
     }
