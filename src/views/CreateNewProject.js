30f479bd9d764202e5331f3816057535ffaa6ea1
Apple <trinq110902@gmail.com>
2023-07-31 09:56:11 +0700

    update

--- src/views/CreateNewProject.js
+++ src/views/CreateNewProject.js
@@ -267,7 +267,7 @@ function CreateNewProject(props) {
                 padding: "2px 10px",
                 marginRight: 8,
               }}>
-              {/* {angle >= 180 ? angle - 180 : 180 + angle} */}
+              {360 - angle >= 180 ? 360 - angle - 180 : 180 + 360 - angle}
               <span>&deg;</span>
             </div>
           </div>
